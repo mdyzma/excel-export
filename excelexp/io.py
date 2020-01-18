c4b31d955cb63a3608448dc013484245634ab1fd
Michal Dyzma <mdyzma@gmail.com>
2020-01-18 20:13:13 +0100

    Added logging msgs

--- excelexp/io.py
+++ excelexp/io.py
@@ -1,4 +1,6 @@
 import os
+import logging
+
 import docx
 import pandas as pd
 from glob import glob
@@ -13,7 +15,7 @@ def get_file_list(data_path):
 def read_data(data_path: str) -> pd.DataFrame():
     files_xls = get_file_list(data_path)
     df = pd.DataFrame()
-
+    logging.info("Extracting data from {} files".format(len(files_xls)))
     for file in tqdm(files_xls):
         data = pd.read_excel(file, header=None, names=["tekst", "wartosc"])
         df = df.append(data)
@@ -24,16 +26,19 @@ def save_results(data, destination: str) -> None:
     doc = docx.Document()
     t = doc.add_table(data.shape[0] + 1, data.shape[1])
 
+    time_now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    fname = "-".join(["results", time_now, ".docx"])
+    path = os.path.join(destination, fname)
+
+    logging.info("Writing files to {} files".format(destination))
+
     # add the header rows.
     for j in range(data.shape[-1]):
         t.cell(0, j).text = data.columns[j]
 
     # add the rest of the data frame
-    for i in range(data.shape[0]):
+    for i in tqdm(range(data.shape[0])):
         for j in range(data.shape[-1]):
             t.cell(i + 1, j).text = str(data.values[i, j])
 
-    time_now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    fname = "-".join(["results", time_now, ".docx"])
-    path = os.path.join(destination, fname)
     doc.save(path)
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
+import logging
 
 from excelexp.cli import create_parser
 from excelexp.io import read_data, save_results
 from excelexp.transform import get_samples  # , filter_rows, filter_func
 
+# set logging
+FORMAT = '%(asctime)-15s:  %(message)s'
+
 
 if __name__ == "__main__":
 
+    logging.basicConfig(level=logging.INFO, format=FORMAT)
+    
     parser = create_parser()
     args = parser.parse_args()
 
