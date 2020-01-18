5833cebf6187be0fa3a48031fde762c8a8de4b05
Michal Dyzma <mdyzma@gmail.com>
2020-01-18 17:48:34 +0100

    Update main.py to execute workflow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from excelexp.io import read_data, save_results
-from excelexp.transform import get_samples
+import os
+
 from excelexp.cli import create_parser
+from excelexp.io import read_data, save_results
+from excelexp.transform import get_samples, filter_rows, filter_func
 
 
 if __name__ == "__main__":
@@ -8,7 +10,13 @@ if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
 
-    data = read_data(args.input, args.nsamples)
+    inputs = os.path.abspath(args.input)
+    data = read_data(inputs)
 
-    sample = get_samples(data)
-    save
+    n_samples = int(args.nsamples)
+    samples = get_samples(data, n_samples)
+
+    filtered_data = filter_rows(samples, filter_func=filter_func)
+
+    destination = os.path.abspath(args.destination)
+    save_results(filtered_data, destination)
